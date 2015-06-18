5627ddb6de0529e70c4c0889d421235017346fbf
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-06-18 02:04:50 +0200

    First version of my Example R Package

---
+++ R/suma.dos.numeros.R
@@ -0,0 +1,21 @@
+#' suma.dos.numeros Function
+#'
+#' This function allows you sum to numbers 
+#' @param value a and value b
+#' @keywords suma.dos.numeros
+#' @export
+#' @examples
+#' suma.dos.numeros()
+
+
+suma.dos.numeros <- function(a,b) {
+
+  result = sum(a,b)
+  
+  print(result)
+  
+}
+
+#Test
+#suma.dos.numeros(2,1)
+
